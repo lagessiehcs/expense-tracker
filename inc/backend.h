3c979e15cb31ef1158e58bb7b73e4da185e1d1e9
quân <116118843+lagessiehcs@users.noreply.github.com>
2024-03-24 12:49:54 +0100

    delete declaration of function edit_expenses()

--- inc/backend.h
+++ inc/backend.h
@@ -13,6 +13,5 @@ void add_user_to_group(User &, Group &);
 void remove_user_from_group(User &user, Group &group);
 unsigned add_group(std::string, std::unordered_map<unsigned, Group> &);
 Expense add_expense_to_group(unsigned amount, unsigned payer_id, std::vector<unsigned> payee_ids, Group &group, std::unordered_map<unsigned, User> &user_list);
-void edit_expense(Group &, unsigned, unsigned);
 
 #endif
